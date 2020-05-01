03e5165e6565c48ab0457052f7101d9be21a1785
Evan <mcintire.evan@gmail.com>
2020-04-30 20:08:14 -0400

    actually fix cors

--- lambda_function.py
+++ lambda_function.py
@@ -9,26 +9,37 @@ def lambda_handler(event, context):
             'Access-Control-Allow-Methods': 'GET, POST, OPTIONS'
         }
     }
-
+    
     if not 'body' in event:
-        return err_resp
-
-
+        resp = {
+            'statusCode': 200,
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, POST, OPTIONS'
+            }
+        }
+        return resp
+        
+    
     ev = json.loads(event['body'])
     if not (('password' in ev) and ('origin' in ev)):
         return err_resp
-
+    
     pa = ev['password']
     ori = ev['origin']
-
+    
     if pa != os.environ['password']:
         return err_resp
-
+        
     url = os.environ['index'] if ori == 'index' else os.environ['join']
-
+    
     response = {
         'statusCode': 200,
-        'body': url
+        'body': url,
+        'headers': {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST, OPTIONS'
+        }
     }
-
+     
     return response
